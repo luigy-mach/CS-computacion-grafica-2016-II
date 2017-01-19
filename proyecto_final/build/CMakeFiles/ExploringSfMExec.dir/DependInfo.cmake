ea8636ccaf1722d56d86eed4c28a1f9b1dc0c4f4
luigy-mach <luigy.mach.arc@gmail.com>
2017-01-18 22:18:38 -0500

    proyecto final

---
+++ proyecto_final/build/CMakeFiles/ExploringSfMExec.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/Visualization.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMExec.dir/Visualization.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/main.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMExec.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_RSSDK"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "USE_EIGEN"
+  "qh_QHpointer"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/vtk-5.10"
+  "/usr/include/eigen3"
+  "/home/luigy/opencv-2.4.9/build"
+  "/home/luigy/opencv-2.4.9/include"
+  "/home/luigy/opencv-2.4.9/include/opencv"
+  "/home/luigy/opencv-2.4.9/modules/core/include"
+  "/home/luigy/opencv-2.4.9/modules/flann/include"
+  "/home/luigy/opencv-2.4.9/modules/imgproc/include"
+  "/home/luigy/opencv-2.4.9/modules/highgui/include"
+  "/home/luigy/opencv-2.4.9/modules/features2d/include"
+  "/home/luigy/opencv-2.4.9/modules/calib3d/include"
+  "/home/luigy/opencv-2.4.9/modules/ml/include"
+  "/home/luigy/opencv-2.4.9/modules/video/include"
+  "/home/luigy/opencv-2.4.9/modules/legacy/include"
+  "/home/luigy/opencv-2.4.9/modules/objdetect/include"
+  "/home/luigy/opencv-2.4.9/modules/photo/include"
+  "/home/luigy/opencv-2.4.9/modules/gpu/include"
+  "/home/luigy/opencv-2.4.9/modules/ocl/include"
+  "/home/luigy/opencv-2.4.9/modules/nonfree/include"
+  "/home/luigy/opencv-2.4.9/modules/contrib/include"
+  "/home/luigy/opencv-2.4.9/modules/stitching/include"
+  "/home/luigy/opencv-2.4.9/modules/superres/include"
+  "/home/luigy/opencv-2.4.9/modules/ts/include"
+  "/home/luigy/opencv-2.4.9/modules/videostab/include"
+  "/home/luigy/opencv-2.4.9/modules/viz/include"
+  "/usr/include/pcl-1.8"
+  "../SSBA/build/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ proyecto_final/build/CMakeFiles/ExploringSfMLibrary.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/AbstractFeatureMatcher.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/AbstractFeatureMatcher.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/BundleAdjuster.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/BundleAdjuster.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/Common.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/Common.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/Distance.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/Distance.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/FindCameraMatrices.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/FindCameraMatrices.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/MultiCameraDistance.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/MultiCameraDistance.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/MultiCameraPnP.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/MultiCameraPnP.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/OFFeatureMatcher.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/OFFeatureMatcher.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/RichFeatureMatcher.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/RichFeatureMatcher.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/SfMUpdateListener.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/SfMUpdateListener.cpp.o"
+  "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/Triangulation.cpp" "/home/luigy/Desktop/grafica/Chapter4_StructureFromMotion/build/CMakeFiles/ExploringSfMLibrary.dir/Triangulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_RSSDK"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "USE_EIGEN"
+  "qh_QHpointer"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/vtk-5.10"
+  "/usr/include/eigen3"
+  "/home/luigy/opencv-2.4.9/build"
+  "/home/luigy/opencv-2.4.9/include"
+  "/home/luigy/opencv-2.4.9/include/opencv"
+  "/home/luigy/opencv-2.4.9/modules/core/include"
+  "/home/luigy/opencv-2.4.9/modules/flann/include"
+  "/home/luigy/opencv-2.4.9/modules/imgproc/include"
+  "/home/luigy/opencv-2.4.9/modules/highgui/include"
+  "/home/luigy/opencv-2.4.9/modules/features2d/include"
+  "/home/luigy/opencv-2.4.9/modules/calib3d/include"
+  "/home/luigy/opencv-2.4.9/modules/ml/include"
+  "/home/luigy/opencv-2.4.9/modules/video/include"
+  "/home/luigy/opencv-2.4.9/modules/legacy/include"
+  "/home/luigy/opencv-2.4.9/modules/objdetect/include"
+  "/home/luigy/opencv-2.4.9/modules/photo/include"
+  "/home/luigy/opencv-2.4.9/modules/gpu/include"
+  "/home/luigy/opencv-2.4.9/modules/ocl/include"
+  "/home/luigy/opencv-2.4.9/modules/nonfree/include"
+  "/home/luigy/opencv-2.4.9/modules/contrib/include"
+  "/home/luigy/opencv-2.4.9/modules/stitching/include"
+  "/home/luigy/opencv-2.4.9/modules/superres/include"
+  "/home/luigy/opencv-2.4.9/modules/ts/include"
+  "/home/luigy/opencv-2.4.9/modules/videostab/include"
+  "/home/luigy/opencv-2.4.9/modules/viz/include"
+  "/usr/include/pcl-1.8"
+  "../SSBA/build/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
